e2c63c01bfac46b7ec2d0d4e52a0184041933c68
rsorbo <robsorbo@gmail.com>
2021-11-01 21:19:45 -0500

    JS Lab done

---
+++ JScript Lab/scripts.js
@@ -0,0 +1,3 @@
+//document.write("  <footer class='text-light bg-dark'>Thank you for visiting my page!</footer> ")
+
+document.getElementById("theFooter").innerHTML = "<footer class='text-light bg-dark'>Thank you for visiting my page!</footer>"
