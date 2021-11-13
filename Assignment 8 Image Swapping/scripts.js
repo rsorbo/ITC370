3b5cc091ebf7d7dfb736c2a182da2734f60471c3
rsorbo <robsorbo@gmail.com>
2021-11-13 17:59:55 -0600

    Picked pics

--- Assignment 8 Image Swapping/scripts.js
+++ Assignment 8 Image Swapping/scripts.js
@@ -2,24 +2,39 @@
 
 
 //Bonus #1
-var imgArray = ["pexels-chevanon-photography-302899.jpg","pexels-chevanon-photography-324028.jpg",
- "pexels-burst-374885.jpg" , "pexels-lood-goosen-1235706.jpg"]
-    
+var images = ["pexels-chevanon-photography-302899.jpg","pexels-chevanon-photography-324028.jpg",
+ "pexels-burst-374885.jpg" , "pexels-burst-374147.jpg"]
+var index = 0;    
 
 
 //Bonus #2
  var target = document.getElementById("target");
-imgArray.forEach(element => {
-    var img = document.createElement("img");
     
-    img.height = '200';
+ images.forEach(element => {
+ var img = document.createElement("img");
+  
+    img.width = '250';
     img.src = element;
-    document.getElementById("target").appendChild(img);
+    target.appendChild(img);
 });
 
+    var main = document.getElementById("main");
+    var mainImage = document.createElement('mainimage')
 
-function nextImage(element)
+    mainImage.width = '1200';
+    mainImage.src = images[index];
+    main.appendChild(mainImage);
+   
+
+
+
+function changePic()
 {
-    document.getElementById("imgArray").src = imgArray[0];
+    index ++
+    if (index == images.length){index = 0;};
+
+    main.appendChild(images[index]);
+    //document.images[0].src = images[index].src;
 }
 
+main.addEventListener("dblclick", changePic);
