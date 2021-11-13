90b9326f60f8c7a064ae26a43d2e35a1510faa1d
rsorbo <robsorbo@gmail.com>
2021-11-13 11:24:20 -0600

    By golly, I think I'm getting somewhere

--- Assignment 8 Image Swapping/scripts.js
+++ Assignment 8 Image Swapping/scripts.js
@@ -1,41 +1,25 @@
-var imgArray = new Array();
 
-imgArray[0]= new Image();
-imgArray[0].src = 'pexels-chevanon-photography-302899.jpg';
 
-imgArray[1]= new Image();
-imgArray[1].src = 'pexels-chevanon-photography-324028.jpg';
 
-imgArray[2]= new Image();
-imgArray[2].src = 'pexels-burst-374885.jpg';
+//Bonus #1
+var imgArray = ["pexels-chevanon-photography-302899.jpg","pexels-chevanon-photography-324028.jpg",
+ "pexels-burst-374885.jpg" , "pexels-lood-goosen-1235706.jpg"]
+    
 
-imgArray[3]= new Image();
-imgArray[3].src = 'pexels-lood-goosen-1235706.jpg';
 
+//Bonus #2
+ var target = document.getElementById("target");
+imgArray.forEach(element => {
+    var img = document.createElement("img");
+    
+    img.height = '200';
+    img.src = element;
+    document.getElementById("target").appendChild(img);
+});
 
 
 function nextImage(element)
 {
-    document.getElementById(imgArray);
-
-    for (var i=0; i<imgArray.length; i++)
-{
-    if(imgArray[i].src==img.src)
-        {
-            if(i=== imgArray.length)
-            {
-            document.getElementById("arrayimages").src = imgArray[0].src;
-            break;
-            }
-        }    
-            document.getElementById(imgArray).src = imgArray[i+1].src;
-            break;
-        }        
+    document.getElementById("imgArray").src = imgArray[0];
 }
-
-const thumb = document.querySelector('div');
-thumb.addEventListener('dblclick', function (nextimage)
-{
-
-});
 
