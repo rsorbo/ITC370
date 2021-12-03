31bf5a6b85a0d5b6ccd698c9dce81c062ae97c30
rsorbo <robsorbo@gmail.com>
2021-12-02 20:47:54 -0600

    Why aren't these daggum cars moving?

--- Great Race/scripts.js
+++ Great Race/scripts.js
@@ -1,37 +1,66 @@
 
 const car1 = new Object();
-car1.name = Red;
-car1.topSpeed = 10;
-car1.minSpeed = 1;
-car1.distance = 0;
+car1.name = "Red";
+car1.topSpeed = 100;
+car1.minSpeed = 10;
+
 
 const car2 = new Object();
-car2.name = Green;
-car2.topSpeed = 8;
-car2.minSpeed = 2;
-car2.distance =0;
+car2.name = "Green";
+car2.topSpeed = 80;
+car2.minSpeed = 30;
+
 
 var redCar = document.getElementById("red");
 var greenCar = document.getElementById("green");
 
-while (car1.distance<100 || car2.distance<100)
-{
-    var move1 = Math.random(car1.minSpeed, car1.topSpeed)
-    var move2 = Math.random(car2.minSpeed, car2.topSpeed)
+var move1 = 0;
+var move2 =0;
 
-    redCar.style.left = (move1) + 'px';
-    greenCar.style.left = (move2) + 'px';
-    car1.distance += move1;
-    car2.distance += move2;
-    setTimeout(500);
+//seeing if I can make it work by simplifying ... only random instead of looking at cars' min/max
+function moveRand (){
+    move1 = Math.random(25, 90) + 'px';
+    move2 = Math.random(25.90) + 'px';
+
+    redCar.style.left += move1;
+    greenCar.style.left += move2;
 }
 
-if (car1.distance >= 100)
-{
-    document.write(car1.name + ' Wins!');
+function redMove(redCar){
+    
+    redCar.style.left += (move1 + 'px');
+    
 }
-if (car2.distance >= 100)
-{
-    document.write(car2.name + ' Wins!');
+function greenMove(greenCar){
+    greenCar.style.left +=  (move2 + 'px');
+    
 }
 
+
+function LetsGo(){
+    document.getElementById('stop').src = "go.png";
+
+    while (redCar.style.left <600 || greenCar.style.left <600)
+    {
+        // move1 = Math.random(car1.minSpeed, car1.topSpeed);
+        // move2 = Math.random(car2.minSpeed, car2.topSpeed);
+    
+        // redMove();
+        // greenMove();
+        moveRand(); 
+
+        setTimeout(500);
+    }
+    
+    // if (car1.distance >= 100)
+    // {
+    //     document.write(car1.name + ' Wins!');
+    // }
+    // if (car2.distance >= 100)
+    // {
+    //     document.write(car2.name + ' Wins!');
+    // }
+
+
+
+}
