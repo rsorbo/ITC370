7cf34734ff436815935e200191ccfa6e28f7be52
rsorbo <robsorbo@gmail.com>
2021-12-02 22:14:09 -0600

    Wait a second, is this working?

--- Great Race/scripts.js
+++ Great Race/scripts.js
@@ -1,66 +1,61 @@
 
 const car1 = new Object();
 car1.name = "Red";
-car1.topSpeed = 100;
-car1.minSpeed = 10;
-
+car1.distance = 0;
 
 const car2 = new Object();
 car2.name = "Green";
-car2.topSpeed = 80;
-car2.minSpeed = 30;
-
+car2.distance = 0;
 
 var redCar = document.getElementById("red");
 var greenCar = document.getElementById("green");
 
 var move1 = 0;
-var move2 =0;
+var move2 = 0;
+max = 5;
 
-//seeing if I can make it work by simplifying ... only random instead of looking at cars' min/max
-function moveRand (){
-    move1 = Math.random(25, 90) + 'px';
-    move2 = Math.random(25, 90) + 'px';
+const distanceToGo = 800;
 
-    redCar.style.left += move1;
-    greenCar.style.left += move2;
-}
-
-function redMove(redCar){
-    
-    redCar.style.left += (move1 + 'px');
-    
-}
-function greenMove(greenCar){
-    greenCar.style.left +=  (move2 + 'px');
-    
-}
 
 
 function LetsGo(){
     document.getElementById('stop').src = "go.png";
 
-    while (redCar.style.left <600 || greenCar.style.left <600)
-    {
-        // move1 = Math.random(car1.minSpeed, car1.topSpeed);
-        // move2 = Math.random(car2.minSpeed, car2.topSpeed);
-    
-        // redMove();
-        // greenMove();
-        moveRand(); 
+    moveRand();
 
-        setTimeout(300);
+    }
+
+    function getRandomNumber(max){
+        return Math.floor((Math.random() * max));
     }
     
-    // if (car1.distance >= 100)
-    // {
-    //     document.write(car1.name + ' Wins!');
-    // }
-    // if (car2.distance >= 100)
-    // {
-    //     document.write(car2.name + ' Wins!');
-    // }
-
-
-
-}
+    //seeing if I can make it work by simplifying ... only random instead of looking at cars' min/max
+    function moveRand (){
+        console.log ("starting max speeds ", max)
+        move1 = getRandomNumber(max);
+        move2 = getRandomNumber (max);
+    
+        console.log ("Red Car starting distance: ", car1.distance)
+        console.log ("Green Car starting distance: ", car2.distance)
+    
+        while (car1.distance < distanceToGo || car2.distance < distanceToGo)
+         {
+        car1.distance = car1.distance + move1;
+        car2.distance =  car2.distance + move2;
+        redCar.style.left = car2.distance + "px";
+        greenCar.style.left = car2.distance + "px";
+        
+        
+        max++;
+    
+        setTimeout(300);
+    
+            console.log ("random numbers: ", move1, move2);
+            console.log ("Red Car ending distance: ", car1.distance)
+            console.log ("Green Car ending distance: ", car2.distance)
+            console.log ("speeds min,max" ,  max)
+        move1 = 0;
+        move2 = 0;
+            console.log ("these should be 0: ", move1, move2);
+        }
+    }
